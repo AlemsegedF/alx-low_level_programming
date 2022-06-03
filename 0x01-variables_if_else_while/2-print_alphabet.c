7a5a2fd4451d0a69c5cc7cddd9f3f9c9d8da5628
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-03 17:12:44 +0000

    created 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *This function prints the alphabet
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int alpha = a;
+
+	while (alpha < alpha + 26)
+	{
+		putchar("%c\n", alpha);
+		alpha = alpha + 1;
+	};
+	return (0);
+}
+
