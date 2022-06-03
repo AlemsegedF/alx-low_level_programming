eed67e5e6592353792de2594ee8d4f7ce839ee36
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-03 20:59:44 +0000

    created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ * This function prints numbers
+ * from less than ten
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num >= 0 && num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n")
+		return (0);
+}
