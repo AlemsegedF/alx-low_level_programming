aa865c9de117c761195f69a735292783afa698dc
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-03 20:21:51 +0000

    created 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *This function prints all the alphabet
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int ca = 'A'
+
+	while (ch > 96 && ch < 123)
+	{
+		putchar(ch);
+		ch = ch + 1;
+	};
+	while (ca > 'A' && ca < (ca + 26))
+	{
+		putchar(ca);
+	}
+		putchar('\n');
+	return (0);
+}
+
