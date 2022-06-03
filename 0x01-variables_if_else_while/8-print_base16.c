69e4dab11cdc38d2035877baed25babd29d1780d
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-03 21:55:23 +0000

    created 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ * This function prints numbers
+ * from less than ten
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num = '0';
+	int ch = 'a';
+
+	while (num >= '0' && num <= '9')
+	{
+		putchar(num);
+		num++;
+	};
+	while (ch >= 'a' && ch <= 'e')
+	{
+		putchar(ch);
+		ch++;
+	};
+	putchar('\n');
+		return (0);
+}
