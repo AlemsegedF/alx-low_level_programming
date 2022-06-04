8c1333c7317bffcb7bec062c5deb7dba7339f19c
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-04 14:54:08 +0000

    created 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,25 +10,19 @@ int main(void)
 
 	for (n1 = 0; n1 <= 98; n1++)
 	{
-		for (n2 = n2 + 1; n2 <= 99; n2++)
+		for (n1 = n2 + 1; n2 <= 99; n2++)
 		{
 			putchar((n1 / 10) + '0');
 			putchar((n2 % 10) + '0');
 			putchar(' ');
 			putchar((n2 / 10) + '0');
 			putchar((n2 % 10) + '0');
-			
 			if (n1 == 98 && n2 == 99)
-					{
-					break;
-					}
-					else
-					{
-					putchar(',');
-					putchar(' ');
-					};
-		};
-	};
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
