adb37d8f3fe2418ec8022195eb3d4504b3f141f5
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-04 09:21:08 +0000

    created 101-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery point
+ *This function prints all the possible
+ *combination of two numbers
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int a = '0';
+
+	for (; a >= '0' && a <= '8'; a++)
+	{
+		for (int b = '0'; b >= '0' && b <= '9'; b++)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != '8' || b != '9')
+				{
+				putchar(',');
+				putchar(' ');
+				};
+			};
+
+		};
+	};
+return (0);
+}
