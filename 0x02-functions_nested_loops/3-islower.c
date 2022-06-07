657358b3936aa0c6341fa174bc46fdb6b90920d4
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-07 19:47:06 +0000

    created 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - Entery point
+ * @c: the integer value it receives
  * Description - this program checks for lowercase character
  * Return: 1 if c is lowercase: 0 otherwise
  */
