19523feb0ddd42ef62a68783510b0a812839eeee
Alemseged Fekede <alemseged.fekede@gmail.com>
2022-06-08 16:37:26 +0000

    edited 100-time-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,40 +16,39 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-			       p = i * j;
-			       p100 = p / 100;
-			       p10 = (p / 10) % 10;
-			       p1 = p % 10;
-			       if (p > 99)
-			       {
-				       _putchar(',');
-				       _putchar(32);
-				       _putchar(p100 + '0');
-				       _putchar(p10 + '0');
-				       _putchar(p1 + '0');
-			       }
-			       else if (p > 9)
-			       {
-				       _putchar(',');
-				       _putchar(32);
-				       _putchar(32);
-				       _putchar(p10 + '0');
-				       _putchar(p1 + '0');
-			       }
-			       else
-			       {
-				       if (j != 0)
-				       {
-					       _putchar(',');
-					       _putchar(32);
-					       _putchar(32);
-					       _putchar(32);
-				       }
-				       _putchar(p + '0');
-			       }
-			}
+				p = i * j;
+				p100 = p / 100;
+				p10 = (p / 10) % 10;
+				p1 = p % 10;
+				if (p > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(p100 + '0');
+					_putchar(p10 + '0');
+					_putchar(p1 + '0');
+				};
+				else if (p > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(p10 + '0');
+					_putchar(p1 + '0');
+				};
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					};
+					_putchar(p + '0');
+				};
+			};
 			_putchar('\n');
-		}
-	}
+		};
+	};
 }
-
