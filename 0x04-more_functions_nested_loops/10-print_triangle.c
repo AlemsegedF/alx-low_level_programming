99c45628586b58048b65cf55550abfc5ca7f788a
Alemseged Fekede <alemseged.fekede@gmail.com>
2022-06-09 16:58:43 +0000

    created 10-print_triangles.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - Entery point
+ * description - prints triangle
+ * @size: entered value
+ * return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = size; i > 0; i--)
+	{
+		for (j = i - 1; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = i; k <= size; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
