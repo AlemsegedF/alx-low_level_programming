dbe6ae46e955effb665f9eedc1923cbe839e2e66
Alemseged Fekede <alemseged.fekede@gmail.com>
2022-06-10 11:55:09 +0000

    created 100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,21 +13,21 @@ int main(void)
 	long int n, i, max;
 
 	n = 612852475143;
-       	max = -1;
-	while(n % 2 == 0)
+	max = -1;
+	while (n % 2 == 0)
 	{
 		max = 2;
 		n = n / 2;
 	};
-	for(i = 3; i <= sqrt(n); i=i+2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		while(n % i == 0)
+		while (n % i == 0)
 		{
 			max = i;
-			n = n/i;
+			n = n / i;
 		}
 	};
-	if(n > 2)
+	if (n > 2)
 	{
 		max = n;
 	}
