bc7320b85b4df83f84fd916f6fc663c81d56f838
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-13 18:51:23 +0000

    created 7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * puts_half - prints half of string
+ * @str: string
+ * return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, half;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	half = i / 2;
+	if (i % 2 == 0)
+                {
+                        _putchar(str[half]);
+                }
+	j = half + 1;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
+
