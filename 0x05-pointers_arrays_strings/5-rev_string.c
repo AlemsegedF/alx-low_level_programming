21cc596da603682cb7038c83110c213fa935af8d
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-13 16:09:29 +0000

    created 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverse string
+ * @s: accepted string
+ * return: reversed string
+ */
+
+void rev_string(char *s)
+{
+	int j, half, max, length = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+	max = length - 1;
+	half = length / 2;
+	j = 0;
+	while (j <= half)
+	{
+		temp = s[max - j];
+		s[max - j] = s[j];
+		s[j] = temp;
+		j++;
+	}
+}
