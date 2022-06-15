fce4b800dba4b5b9aeb0f2db8b0ff3c83c923359
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-15 17:03:14 +0000

    edited 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,31 +8,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	signed int result;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-		if (s1[i] == s2[i])
-		{
-			result = 0;
-			continue;
-		}
-		else if (s1[i] > s2[i])
-		{
-			result = 15;
-			break;
-		}
-		else if (s1[i] < s2[i])
-		{
-			result = -15;
-			break;
-		}
-		else
-		{
-			result = 0;
-			break;
-		}
+		s1++;
+		S2++;
 	}
-	return (result);
+	return (*s1 - *s2);
 }
