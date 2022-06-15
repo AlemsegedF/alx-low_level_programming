0dd99f44d8aed67da12bc21a2d22af32d9e97748
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-15 16:31:08 +0000

    edited 6-caps.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,7 @@
 char *cap_string(char *str)
 {
 	int i, j;
-	char cm[13] = {',', ';', '.', '!', '?', '"',
-	'(', ')', '{', '}', ' ', '\n', '\t'};
+	char cm[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -22,8 +21,6 @@ char *cap_string(char *str)
 				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 					str[i + 1] -= 32;
 		}
-		if (str[i] == '\t')
-			str[i] = ' ';
 	}
 	return (str);
 }
