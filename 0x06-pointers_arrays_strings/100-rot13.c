ef7e44905b892234403279dea8cc735f8a3bd8d8
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-16 13:05:04 +0000

    created 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * rot13 -encods a string using rot13
+ * @s: sting to be encoded
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/**while((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))**/
+		{
+			if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+			{
+				s[i] += 13;
+			}
+			else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+			{
+				s[i] -= 13;
+			}
+		}
+	}
+	return (s);
+}
