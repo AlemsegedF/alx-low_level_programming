a83f3a6576c4dd7af11d7ffdd2c03d064a1720be
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-16 12:18:21 +0000

    created 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,12 +3,25 @@
 
 /**
  * leet - swaps letters {aeotl} by {43071}
- * @str: string to be encoded
+ * @s: string to be encoded
  * Return: encoded string
  */
-
-char *leet(char *)
+char *leet(char *s)
 {
-	int i;
+	int j, k;
+	char str1[5] = {'a', 'e', 'o', 't', 'l'};
+	char str2[5] = {'A', 'E', 'O', 'T', 'L'};
+	int cod[5] = {4, 3, 0, 7, 1};
 
-
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (k = 0; k < 5; k++)
+		{
+			if (s[j] == str1[k] || s[j] == str2[k])
+			{
+				s[j] = cod[k] + '0';
+			}
+		}
+	}
+	return (s);
+}
