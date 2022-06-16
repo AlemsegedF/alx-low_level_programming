dfd1c7ac0f0831d1b5f4adc47e1a6c77c26001e6
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-16 17:42:11 +0000

    created 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,18 +9,14 @@
 
 void print_number(int n)
 {
-	int m;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -1 * n;
 	}
-	m = n;
-	if (n > 9)
+	if (n / 10 > 0)
 	{
-		n = n / 10;
-		print_number(n);
+		print_number(n / 10);
 	}
-	_putchar(m % 10 + '0');
+	_putchar((n % 10) + '0');
 }
