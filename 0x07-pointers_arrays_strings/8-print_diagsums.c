0af3f8005d5d7302de4ae8d63f822ccef7165d9c
AlemsegedF <alemseged.fekede@gmail.com>
2022-06-20 22:35:23 +0000

    created 8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,11 @@
  * @size: size of array
  * Return: nothing
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
-	
+	int i, j;
+	int sum1 = 0, sum2 = 0;
+
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i];
