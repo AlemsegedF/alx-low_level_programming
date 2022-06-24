a4dba8b24fa4c5fe82e78e913eaacd312ca9a446
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-06-24 09:49:26 -0700

    created 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main -prints the number of arguments passed to it
+ * @argc: argumnts count
+ * @argv: arguments vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for(i = 1; i < argc; i++)
+	{
+		n = atoi(argv[i]);
+		if (n == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
