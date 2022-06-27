578ae7c38756464929f5054ecc63403ae153a66a
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-06-27 02:38:41 -0700

    edited 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2, size;
 	char *array;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = i = j = len2 = size = 0;
 	for (i = 0; s1[i]; i++)
 		len1++;
