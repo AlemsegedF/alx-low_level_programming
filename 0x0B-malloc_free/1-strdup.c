da52be26737ba8e9271ad59cd90311ba6ef704ae
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-06-27 00:04:42 -0700

    edited 1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,10 +18,8 @@ char *_strdup(char *str)
 	size = sizeof(str);
 	if (size == 0)
 		return (NULL);
-	array = (char *) malloc(size * sizeof(char));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
+	array = (char *) malloc((size + 1)* sizeof(char));
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		array[i] = str[i];
 	}
