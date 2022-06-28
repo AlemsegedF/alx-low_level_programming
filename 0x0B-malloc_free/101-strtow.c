c84817fbcf3cbf0c3238888ce076630b692037a0
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-06-28 13:18:43 -0700

    last modified 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,39 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char **memory_l(char *);
+int num_words_func(char *);
+int word_size(char *);
+
 /**
- * memory_l - create double pointer using malloc
+ * num_words_func - count words
  * @str: string to be split
- * Return: ptr
+ * Return: num_words
  */
 
-char **memory_l(char *str)
+int num_words_func(char *str)
 {
-	long int i, num_words, len = 0;
-	char **ptr;
+	int i, num_words, len = 0;
 
-	for(i = 0; str[i]; i++)
-		len++;
-	if (len <= 1)
-		return (NULL);
-	if (str == NULL)
-		return (NULL);
-	num_words = 0;
 	for (i = 0; str[i]; i++)
+		len++;
+	num_words = 0;
+	for (i = 0; i < len; i++)
 	{
-		if (str[i] != ' ' && str[i - 1] == ' ')
+		if (str[i] != ' ')
+		{
 			num_words++;
-		else if (str[i] != ' ' && str[i + 1] == ' ')
-			num_words++;
+			i += word_size(str + i);
+		}
 	}
-	ptr = malloc(sizeof(char *) * num_words);
-	if (ptr == NULL)
+	return (num_words);
+}
+
+
+/**
+ * size_word - number of characters in word
+ * @str: string
+ * Return: size
+ */
+int word_size(char *str)
+{
+	int i, size = 0;
+
+	for (i = 0; str[i] && str[i] != ' '; i++)
 	{
-		free(ptr);
-		return (NULL);
+		size++;
 	}
-	return (ptr);
+	return (size);
 }
 
 /**
@@ -46,42 +55,39 @@ char **memory_l(char *str)
 
 char **strtow(char *str)
 {
-	long i, j, k, size, ptr_i;
+	long i, j, num_words, size, ptr_i;
 	char **ptr;
 
-	ptr = memory_l(str);
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	num_words = num_words_func(str);
+	if (num_words == 0)
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (num_words + 1));
 	if (ptr == NULL)
 		return (NULL);
-	ptr_i = 0;
-	for (i = 0; str[i]; i++)
+	i = 0;
+	for (ptr_i = 0; ptr_i < num_words; ptr_i++)
 	{
-		size = 0;
-		for (k = i; str[k] != ' '; k++)
+		while (str[i] == ' ')
+			i++;
+		size = word_size(str + i);
+		ptr[ptr_i] = malloc(sizeof(char) * (size + 1));
+		if (ptr[ptr_i] == NULL)
 		{
-			size++;
+			for (j = ptr_i; j >= 0; j--)
+			{
+				free(ptr[ptr_i]);
+			}
+			free(ptr);
+			return (NULL);
 		}
-		if (size >= 1)
+		for (j = 0; j < size; j++)
 		{
-			ptr[ptr_i] = malloc(sizeof(char) * (size - 1));
-			if (ptr[ptr_i] == NULL)
-			{
-				for (j = ptr_i; j >= 0; j--)
-				{
-					free(ptr[ptr_i]);
-				}
-				free(ptr);
-				return (NULL);
-			}
-			k = 0;
-			for (j = i; str[j] != ' '; j++)
-			{
-				ptr[ptr_i][k] = str[j];
-				k++;
-			}
-			ptr[ptr_i][k] = '\0';
-			i = j;
-			ptr_i++;
+			ptr[ptr_i][j] = str[i++];
 		}
+		ptr[ptr_i][j] = '\0';
 	}
+	ptr[ptr_i] = NULL;
 	return (ptr);
 }
