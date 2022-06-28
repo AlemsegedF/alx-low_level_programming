f9b26142352eae2043d8994d9a0fc31040821997
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-06-28 07:39:43 -0700

    modified strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 char **memory_l(char *);
 /**
@@ -10,13 +11,21 @@ char **memory_l(char *);
 
 char **memory_l(char *str)
 {
-	int i, num_words;
+	int i, num_words, len= 0;
 	char **ptr;
 
+	for(i = 0; str[i]; i++)
+		len++;
+	if (len == 0)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
 	num_words = 0;
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] != ' ' || *(str + i + 1) == ' ')
+		if (str[i] != ' ' && str[i - 1] == ' ') 
+			num_words++;
+		else if (str[i] != ' ' && str[i + 1] == ' ')
 			num_words++;
 	}
 	ptr = malloc(sizeof(char *) * num_words);
@@ -39,8 +48,10 @@ char **strtow(char *str)
 {
 	int i, j, k, size, ptr_i;
 	char **ptr;
-
+	
 	ptr = memory_l(str);
+	if (ptr == NULL)
+		return (NULL);
 	ptr_i = 0;
 	for (i = 0; str[i]; i++)
 	{
@@ -51,7 +62,7 @@ char **strtow(char *str)
 		}
 		if (size >= 1)
 		{
-			ptr[ptr_i] = malloc(sizeof(char) * (size - 1));
+			ptr[ptr_i] = malloc(sizeof(char) * (size));
 			if (ptr[ptr_i] == NULL)
 			{
 				for (j = ptr_i; j >= 0; j--)
