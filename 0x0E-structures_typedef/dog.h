ada457e9d00415278ca37d2383bc2a3eef451ce3
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-07-04 11:08:50 -0700

    created new  file dog.h and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - info about dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: this struct holds info about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
