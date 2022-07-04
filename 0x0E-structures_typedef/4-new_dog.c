736462b53d8d259052ea9bc0282dff13ebcacacc
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-07-04 14:16:06 -0700

    last modification of 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
-char *_strcpy(char *dest, char *src);
+
+int _strlen(char *);
+char *_strcpy(char *, char *);
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -22,7 +24,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(ptr_dog);
 		return (NULL);
 	}
-	len = strlen(name) + 1;
+	len = _strlen(name) + 1;
 	ptr_dog->name = malloc(sizeof(char) * len);
 	if (ptr_dog->name == NULL)
 	{
@@ -33,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr_dog->name = _strcpy(ptr_dog->name, name);
 	ptr_dog->age = age;
 	len = 0;
-	len = strlen(owner) + 1;
+	len = _strlen(owner) + 1;
 	ptr_dog->owner = malloc(sizeof(char) * len);
 	if (ptr_dog->owner == NULL)
 	{
@@ -61,6 +63,23 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] ='\0';
+	dest[i] = '\0';
 	return (dest);
+}
+
+/**
+ * _strlen - calculate length of string
+ * @str: string to be measured
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+
 }
