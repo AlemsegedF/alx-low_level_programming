a993a7048000ab67ea8ab70f106f86f506ee86d7
vagrant <vagrant@ubuntu2004.localdomain>
2022-07-14 18:45:22 +0000

    created 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void  tortoise(void) __attribute__((constructor));
+/**
+* tortoise - prints a text befor main function is excuted
+* Return: nothing
+*/
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
