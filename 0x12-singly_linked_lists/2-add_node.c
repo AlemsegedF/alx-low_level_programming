ceb0a2319126cb0da08cb287e1df255e4a5e0259
vagrant <vagrant@ubuntu2004.localdomain>
2022-07-14 17:24:22 +0000

    modified 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,7 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	char *temp;
 	int len;
 
-	new_node = (list_t *) malloc(sizeof(list_t))
+	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
