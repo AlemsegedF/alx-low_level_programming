51c8719fb15f6851c8266ce52251ed7abc676c75
root <root@ubuntu2004.localdomain>
2022-07-18 21:25:04 +0000

    created 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,4 @@
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 9-main.c 3-add_nodeint_end.c 0-print_listint.c 5-free_listint2.c 9-insert_nodeint.c -o j#include "lists.h"
+#include "lists.h"
 #include <stdlib.h>
 
 /**
