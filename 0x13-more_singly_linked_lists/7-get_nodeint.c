c8abc15fcbf06bf4a340ff1f9eefec6c776c51f2
root <root@ubuntu2004.localdomain>
2022-07-18 20:17:51 +0000

    created 7-get_node_int.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,9 +16,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	ptr = head;
 	for (i = 0; i < index; i++)
 	{
+		if (ptr == NULL)
+			return (NULL);
 		ptr = ptr->next;
 	}
-	if (ptr == NULL)
-		return (NULL);
 	return (ptr);
 }
