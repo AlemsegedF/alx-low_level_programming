b7613bd38babe1cdc6aaab1a3872dc8608ffd403
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-07-19 08:09:06 -0700

    modified 0-print

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define HEADER_LISTS
 #include <stddef.h>
 /**
-* struct listint+s - singly linked list
+* struct listint_s - singly linked list
 * @n: integer
 * @next: points to next node
 *
@@ -24,6 +24,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);int delete_nodeint_at_index(listint_t **head, unsigned int index);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #endif
 
