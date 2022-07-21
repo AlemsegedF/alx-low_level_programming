0fc74fd5d6bc7399de81d7d3f3d8ad434b7cb340
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-07-21 07:38:10 -0700

    edited

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,6 +12,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum = 0;
 	int i = 0;
 
+	if (b == NULL)
+		return (0);
 	if (b[i] == '\0')
 		return (0);
 	while ((b[i] == '0') || (b[i] == '1'))
