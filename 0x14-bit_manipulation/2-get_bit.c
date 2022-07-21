072279e9834bb2667835d955bf4f5d8184a6e20a
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-07-21 08:50:55 -0700

    created 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,8 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int num;
 
-	if (index >(sizeof(unsigned long int) * 8))
-				return (-1);
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
 	num = ((n >> index) & 1);
 	return (num);
 }
