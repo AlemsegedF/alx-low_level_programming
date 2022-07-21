ecc50bba3d70894ebe3501d47dd8176e6fc5a030
ALemseged Fekede <alemseged.fekede@gmail.com>
2022-07-21 09:10:30 -0700

    created 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 1 at a given index
+ * @n: number
+ * @index: index of the bit
+ * Return: 1 if worked or -1 if an error occured
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	num = ~(1 << index);
+	*n = (num & *n);
+	return (1);
+}
