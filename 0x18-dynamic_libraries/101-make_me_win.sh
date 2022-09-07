68d782eb486929e530aaf35fde16cefd6623a5be
AlemsegedF <alemseged.fekede@gmail.com>
2022-09-07 21:05:13 +0000

    edited

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-wget -q -O /run/librand.so https://github.com/AlemsegedF/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwhat.so
-export LD_PRELOAD=/run/librand.so
+#!/bin/bashD
+wget -p /tmp https://github.com/AlemsegedF/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
+export LD_PRELOAD=/run/libwin.so
